f8d902e5d1f86aaa3833db8eec1e12ab6928223d
Andhika Pratama <advanvandroid5566@gmail.com>
2019-10-13 14:57:42 +0700

    tugas c(listview with onclick)

--- app/src/main/java/cobakotlin/example/com/listviewicon2/MainActivity.kt
+++ app/src/main/java/cobakotlin/example/com/listviewicon2/MainActivity.kt
@@ -7,40 +7,58 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val language    = arrayOf<String>("Ruby","Rails",
-                                                    "Python","Java Script",
-                                                    "Php")
-    val description = arrayOf<String>(
-        "Ruby is an open-source and fully object-oriented programming language.",
-        "Ruby on Rails is a server-side web application development framework written in Ruby language.",
-        "Python is interpreted scripting  and object-oriented programming language.",
-        "JavaScript is an object-based scripting language.",
-        "PHP is an interpreted language, i.e., there is no need for compilation."
-        )
+    val language    = arrayOf<String>("Soekarno","Soeharto",
+                                                    "Habibie","Gusdur",
+                                                    "Megawati","SBY","Jokowi")
 
     val imageId = arrayOf<Int>(
-        R.drawable.ruby,
-        R.drawable.rails,
-        R.drawable.python,
-        R.drawable.java,
-        R.drawable.php
+        R.drawable.soekarno,
+        R.drawable.harto,
+        R.drawable.havivi,
+        R.drawable.gusdur,
+        R.drawable.mega,
+        R.drawable.sby,
+        R.drawable.jokowi
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myListAdapter = MyListAdapter(this,language,description,imageId)
+        val myListAdapter = MyListAdapter(this,language,imageId)
         listView.adapter = myListAdapter
 
         listView.setOnItemClickListener(){adapterView, view, position, id ->
             val itemAtPos = adapterView.getItemAtPosition(position)
             val itemIdAtPos = adapterView.getItemIdAtPosition(position)
-            Toast.makeText(this, "Click on item at $itemAtPos its item id $itemIdAtPos", Toast.LENGTH_LONG).show()
+
+            var a = Integer.parseInt(itemIdAtPos.toString())
+            var pr = a+1
+
+            if (a.equals(0)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else if (a.equals(1)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else if (a.equals(2)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else if (a.equals(3)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else if (a.equals(4)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else if (a.equals(5)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else if (a.equals(6)){
+                Toast.makeText(this, "Ini Bapak $itemAtPos , Presiden ke $pr", Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this, "Pilihan Tidak Ada", Toast.LENGTH_LONG).show()
+            }
         }
-    }
-
-    fun klklkl(){
-
     }
 }
--- app/src/main/java/cobakotlin/example/com/listviewicon2/MyListAdapter.kt
+++ app/src/main/java/cobakotlin/example/com/listviewicon2/MyListAdapter.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-class MyListAdapter(private val context: Activity, private val title: Array<String>, private val description: Array<String>, private val imgid: Array<Int>)
+class MyListAdapter(private val context: Activity, private val title: Array<String>, private val imgid: Array<Int>)
     : ArrayAdapter<String>(context, R.layout.custom_list, title) {
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
@@ -13,11 +13,9 @@ class MyListAdapter(private val context: Activity, private val title: Array<Stri
 
         val titleText = rowView.findViewById(R.id.title) as TextView
         val imageView = rowView.findViewById(R.id.icon) as ImageView
-        val subtitleText = rowView.findViewById(R.id.description) as TextView
 
         titleText.text = title[position]
         imageView.setImageResource(imgid[position])
-        subtitleText.text = description[position]
 
         return rowView
     }
